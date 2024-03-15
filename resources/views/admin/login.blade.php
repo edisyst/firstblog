cd1bc6a227c49334a01b8775c4ff6a36821b48a7
Edoardo Testagrossa <editestar@gmail.com>
2024-03-15 08:28:34 +0100

    lez.17

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -25,22 +25,22 @@
     <!-- End fonts -->
 
     <!-- core:css -->
-    <link rel="stylesheet" href="{{ asset('../../../assets/vendors/core/core.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/core/core.css') }}">
     <!-- endinject -->
 
     <!-- Plugin css for this page -->
     <!-- End plugin css for this page -->
 
     <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('../../../assets/fonts/feather-font/css/iconfont.css') }}">
-    <link rel="stylesheet" href="{{ asset('../../../assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/fonts/feather-font/css/iconfont.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
     <!-- endinject -->
 
     <!-- Layout styles -->
-    <link rel="stylesheet" href="{{ asset('../../../assets/css/demo2/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/demo2/style.css') }}">
     <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="{{ asset('../../../assets/images/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.png') }}" />
 </head>
 <body>
 <div class="main-wrapper">
@@ -64,7 +64,7 @@
                                         @csrf
                                         <div class="mb-3">
                                             <label for="userEmail" class="form-label">Email/Name/Phone</label>
-                                            <input type="text" name="login" class="form-control" placeholder="Email">
+                                            <input type="text" name="loginField" class="form-control" placeholder="Email">
                                         </div>
                                         <div class="mb-3">
                                             <label for="userPassword" class="form-label">Password</label>
@@ -93,15 +93,15 @@
 </div>
 
 <!-- core:js -->
-<script src="{{ asset('../../../assets/vendors/core/core.js') }}"></script>
+<script src="{{ asset('backend/assets/vendors/core/core.js') }}"></script>
 <!-- endinject -->
 
 <!-- Plugin js for this page -->
 <!-- End plugin js for this page -->
 
 <!-- inject:js -->
-<script src="{{ asset('../../../assets/vendors/feather-icons/feather.min.js') }}"></script>
-<script src="{{ asset('../../../assets/js/template.js') }}"></script>
+<script src="{{ asset('backend/assets/vendors/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('backend/assets/js/template.js') }}"></script>
 <!-- endinject -->
 
 <!-- Custom js for this page -->
